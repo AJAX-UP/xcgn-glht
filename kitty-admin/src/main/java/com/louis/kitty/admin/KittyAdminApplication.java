b25f2648fb22893edb591e50ef6df61c44fdc097
czl960612 <18513348915@163.com>
2019-11-29 14:44:29 +0800

    初次提交

--- kitty-admin/src/main/java/com/louis/kitty/admin/KittyAdminApplication.java
+++ kitty-admin/src/main/java/com/louis/kitty/admin/KittyAdminApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication(scanBasePackages={"com.louis.kitty"})
 public class KittyAdminApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KittyAdminApplication.class, args);
 	}
